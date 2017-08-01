d9d119bacc96207fe0dd0207dc5fc6fd8d522177
AnilKumar2087 <a.anil.adunuri@gmail.com>
2017-08-01 23:45:51 +0530

    assgnment

---
+++ src/assignment8_3/Account.java
@@ -0,0 +1,17 @@
+package assignment8_3;
+
+public class Account {
+    int balance= 1000;
+
+    public int getBal(){
+        return balance;
+    }
+
+    public void withdraw(int bal){
+        balance= balance-bal;
+    }
+
+    public void deposit(int bal){
+        balance= balance+bal;
+    }
+}
